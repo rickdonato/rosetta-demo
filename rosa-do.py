af6009178a4b679d5860ef799ba241be99a09f87
packetpimp3 <admin@packetflow.co.uk>
2019-08-31 14:06:24 +0100

    cli updates

--- rosa-do.py
+++ rosa-do.py
@@ -1,34 +1,31 @@
 from ntc_rosetta import get_driver
-import sys
+import argparse
 
-candidate = sys.argv[1]
-running = sys.argv[2]
-nos = sys.argv[3]
+parser = argparse.ArgumentParser(description='Rosetta merge from Yang')
+parser.add_argument('-n','--nos', help='nos', required=True)
+parser.add_argument('-c','--candidate', help='candidate config', required=True)
+parser.add_argument('-r','--running', help='running config', required=True)
+
+
+args = vars(parser.parse_args())
 
 def load_driver(nos):
-    print("load driver")
     nos = get_driver(nos, "openconfig")
     return nos()
 
-def merge_from_yang():
+def merge_yang():
     nos_driver = load_driver(nos)
 
-    print(running)
-    print("open configs (yang)")
     with open(running, "r") as f:
         running_config = f.read()
 
     with open(candidate, "r") as f:
         candidate_config = f.read()
 
-    print("merge")
     config = nos_driver.merge(
         candidate=parsed_candidate.raw_value(),
         running=parsed_running.raw_value()
     )
     return config
 
-def convert_native_to_yang():
-    load_driver()
 
-merge_from_yang()
